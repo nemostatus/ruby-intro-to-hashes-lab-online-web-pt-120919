e0e5a4236af77a1ab4957bb65ae4423f4bdbd947
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-12-19 12:11:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 def monopoly_with_second_tier
 	monopoly = {:railroads => {pieces: 4,
 	names:{},
-	rent_in_dollars:{one_pie}}}
+	rent_in_dollars:{one_piece_owned}}}
 end
 
 def monopoly_with_third_tier
